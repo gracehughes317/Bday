7c8a92e80e5432893e10b8dd184ce12f38d3eae6
Pranay Reddy <19reddyp@elmbrookstudents.org>
2018-06-13 13:10:06 -0500

    added response

--- Bday/Program.cs
+++ Bday/Program.cs
@@ -7,6 +7,7 @@ namespace Bday
         static void Main(string[] args)
         {
             Console.WriteLine("What do you want for your birthday?");
+            Console.WriteLine("Phone");
 
         }
     }
