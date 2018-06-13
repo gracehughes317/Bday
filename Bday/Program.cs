a2cce58df92d4f18ed9b0a865ed4a26cace55033
Unknown <19brunel@elmbrookstudents.org>
2018-06-13 13:28:49 -0500

    Added and removed

--- Bday/Program.cs
+++ Bday/Program.cs
@@ -7,12 +7,12 @@ namespace Bday
         static void Main(string[] args)
         {
             Console.WriteLine("What do you want for your birthday?");
-            Console.WriteLine("stuff");
             Console.WriteLine("Phone");
             Console.WriteLine("I want m$n$y monayyy money");
             Console.WriteLine("The latest gaming laptop");
             Console.WriteLine("Sunglasses");
             Console.WriteLine("A non piece of crap computer.");
+            Console.WriteLine("Pranay ;)");
             Console.ReadKey();
         }
     }
